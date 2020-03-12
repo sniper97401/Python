65363fb7f486e46ba7f1754594ec91ac59ae4685
sniper <thodrula@gmail.com>
2020-03-12 14:50:10 +0100

    start

---
+++ Feature/IdentificationAction.py
@@ -0,0 +1,6 @@
+def definePseudo():
+    print('Veuillez entrer votre pseudo')
+    pseudo = input()
+    while (len(pseudo) < 2):
+        print('Votre pseudo n\'est pas conforme')
+        pseudo = input()
---
+++ Sub.py
@@ -0,0 +1,3 @@
+from Feature.IdentificationAction import definePseudo
+print("Bienvenue sur notre jeu video ")
+pseudo = definePseudo()
